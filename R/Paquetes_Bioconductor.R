da243488430599eed8dda8f156b2529d8e0c36d9
lotmta <lotmateohernandezespinosa@gmail.com>
2023-01-31 14:04:00 -0600

    agrego notas discusion paquetes

---
+++ R/Paquetes_Bioconductor.R
@@ -0,0 +1,29 @@
+#### Paquetes Bioconductor
+
+
+## EasyCellType
+
+## Autores: Ruoxing Li, Ziyi Li
+
+# Este paquete utiliza datos de single cell RNA-seq para mostrar el posible tipo celular de las células
+# secuenciadas, esto se presenta gráficamente con plots mostrando a que tipo celular se parece mas.
+
+# Esta soportado en todas las plataformas y no parece tener ningún error.
+
+# No tiene muchas descargas y no tiene dudas o posts discutiéndolo, lo que señala a que no ha sido
+# usado mucho y puede que tenga varios problemas no descubiertos por el poco uso.
+
+# La documentación parece decente.
+
+
+## ggtree
+
+## Autores: Guangchuang Yu, Shuangbin Xu, Chuanjie Zhang
+
+# Este paquete facilita la creación de dendogramas en r, como para filogenias.
+
+# Soportado en todas las plataformas y no parece tener ningún error.
+
+# Pocas descargas y no hay dudas ni posts discutiéndolo.
+
+# La documentación es buena.
